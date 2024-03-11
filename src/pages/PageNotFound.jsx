8385da1ecbd199ae85e3deb505a105c14affb028
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-03-11 16:33:55 +0530

    made 404 page responsive

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,23 +2,25 @@ import { useSelector } from "react-redux";
 import robot from "../assets/images/404 robot.png";
 
 import barcode from "../assets/images/barcode.png";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
   const mode = useSelector((state) => state.common.mode);
+  const navigate = useNavigate();
   return (
     <>
       <div
-        className={`w-full h-full flex relative justify-center items-center overflow-hidden`}
+        className={`w-full h-full flex relative justify-center items-center overflow-hidden select-none`}
       >
         <p
-          className={` text-[2vw] orbitron absolute top-[10%] left-[10%] ${
+          className={`max-sm:hidden  text-[2vw] orbitron absolute top-[10%] left-[10%] ${
             mode && "text-white"
           } transition-all`}
         >
           It looks
         </p>
         <p
-          className={`text-[2vw] orbitron absolute top-[15%] left-[12%] ${
+          className={`max-sm:hidden text-[2vw] orbitron absolute top-[15%] left-[12%] ${
             mode && "text-white"
           } transition-all`}
         >
@@ -49,18 +51,21 @@ const PageNotFound = () => {
             4
           </p>
           <div
-            className={`z-30 absolute w-[30vw] top-40 right-5 `}
+            className={`max-sm:hidden z-30 absolute w-[30vw] top-40 right-5 `}
           >
             <img className={` w-full`} src={robot} alt="" />
           </div>
         </div>
         <img
-          className={`z-30 absolute w-[15vw] -bottom-10 right-5 ${
+          className={`max-sm:hidden z-30 absolute w-[15vw] -bottom-10 right-5 ${
             mode && "invert"
           }`}
           src={barcode}
           alt=""
         />
+        <button onClick={()=>navigate("/")} className={`cursor-pointer ${mode?"hover:bg-white":"hover:bg-black"} ${mode?"hover:text-black":"hover:text-white"} text-[1vw] max-sm:text-[3vw] orbitron absolute max-sm:left-[50%] max-sm:-translate-x-1/2 bottom-[10%] left-[10%] max-sm:bottom-[33%] z-50 ${
+            mode && "text-white text-center"
+          } transition-all border ${mode?"border-white":"border-black"} rounded-3xl px-8 py-2 max-sm:px-6`}>Travel Home</button>
       </div>
     </>
   );
