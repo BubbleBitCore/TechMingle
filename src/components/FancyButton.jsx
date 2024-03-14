ebb820ecd6888dc64c487e9f33f894963ff6550e
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-03-14 22:20:14 +0530

    updated signup page

--- src/components/FancyButton.jsx
+++ src/components/FancyButton.jsx
@@ -1,16 +1,16 @@
 const FancyButton = ({ text1 = "Button", text2 = "Buttons" }) => {
   return (
     <>
-      <div class="fancyBtnWrapper">
-        <button class="btn">
-          <div class="btn__bg">
-            <span class="btn__bg__layer btn__bg__layer-first"></span>
-            <span class="btn__bg__layer btn__bg__layer-second"></span>
-            <span class="btn__bg__layer btn__bg__layer-third"></span>
+      <div className="fancyBtnWrapper">
+        <button className="btn">
+          <div className="btn__bg">
+            <span className="btn__bg__layer btn__bg__layer-first"></span>
+            <span className="btn__bg__layer btn__bg__layer-second"></span>
+            <span className="btn__bg__layer btn__bg__layer-third"></span>
           </div>
 
-          <span class="btn__text-out">{text1}</span>
-          <span class="btn__text-in">{text2}</span>
+          <span className="btn__text-out select-none">{text1}</span>
+          <span className="btn__text-in select-none">{text2}</span>
         </button>
       </div>
     </>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import man2 from "../assets/images/man2.png";
 import FancyButton from "../components/FancyButton";
 
@@ -7,22 +8,24 @@ const Signup = () => {
       <div className="fixed top-0 left-0 w-full h-full flex justify-center  items-center  bgGradient">
         <div className="w-[70%] h-[70vh] rounded-2xl flex justify-center items-center">
           {/* section 1 */}
-          <div className="w-[60%] flex justify-center items-center h-full rounded-2xl  border-2 border-r-0 rounded-r-none  border-[#39196D] relative z-20">
-            <div className="flex  flex-col justify-center items-center ">
-              <i className="ri-vip-crown-2-fill  relative z-20 bg-gradient-to-r from-indigo-300 to-purple-400 text-transparent bg-clip-text text-8xl mb-3"></i>
-              <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-6xl font-bold monsterrat mb-3">
-                TECHMINGLE
-              </p>
-              <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-6xl font-bold monsterrat mb-3">
-                FALL-ONLINE
-              </p>
-              <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-6xl font-bold monsterrat mb-3">
-                SIGNUP-FREE
-              </p>
-            </div>
-            {/* Text bg */}
-            <div className="absolute overflow-hidden top-0 h-full w-full left-0 outlinedText z-10 orbitron text-[50vw] ">
-              <i className="absolute -top-48 left-[30%] ri-gatsby-line opacity-85  "></i>
+          <div className="glowBlock w-[60%] h-full rounded-2xl p-[0.2rem] pr-0">
+            <div className=" w-full flex justify-center items-center h-full rounded-2xl   border-r-0 rounded-r-none  bg-[#210C27] relative z-20">
+              <div className="flex  flex-col justify-center items-center ">
+                <i className="ri-vip-crown-2-fill  relative z-20 bg-gradient-to-r from-indigo-300 to-purple-400 text-transparent bg-clip-text text-[5vw] mb-3"></i>
+                <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-[4vw] leading-[1] font-bold monsterrat mb-3 select-none">
+                  TECHMINGLE
+                </p>
+                <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-[4vw] leading-[1] font-bold monsterrat mb-3 select-none">
+                  FALL-ONLINE
+                </p>
+                <p className="uppercase text-center relative z-20 bg-gradient-to-r from-violet-700 to-purple-300 via-indigo-400 text-transparent bg-clip-text text-[4vw] leading-[1] font-bold monsterrat mb-3 select-none">
+                  SIGNUP-FREE
+                </p>
+              </div>
+              {/* Text bg */}
+              <div className="absolute overflow-hidden top-0 h-full w-full left-0 outlinedText z-10 orbitron text-[50vw] ">
+                <i className="absolute -top-48 left-[30%] ri-gatsby-line opacity-85  "></i>
+              </div>
             </div>
           </div>
           {/* section 2 */}
@@ -36,13 +39,13 @@ const Signup = () => {
                     <img src={man2} alt="" />
                   </div>
                   <div className="flex flex-col items-end">
-                    <p className="text-lg poppins ">ADMITS</p>
-                    <p className="font-bold  text-4xl poppins">01</p>
+                    <p className="text-lg poppins select-none">ADMITS</p>
+                    <p className="font-bold  text-4xl poppins select-none">01</p>
                   </div>
                 </div>
                 <div className="w-full overflow-hidden">
                   <input
-                    className="border-none outline-none text-3xl poppins font-bold"
+                    className="select-none border-none outline-none text-3xl poppins font-bold"
                     type="text"
                     placeholder="Name"
                   />
@@ -57,33 +60,36 @@ const Signup = () => {
               {/* Other fields */}
               <div className="w-full overflow-hidden mb-3">
                 <input
-                  className="border-none w-full outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1 "
+                  className="select-none border-none w-full outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1 "
                   type="text"
                   placeholder="Phone"
                 />
               </div>
               <div className="w-full overflow-hidden mb-3">
                 <input
-                  className="border-none outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1   "
+                  className="select-none border-none outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1   "
                   type="text"
                   placeholder="Email"
                 />
               </div>
               <div className="w-full overflow-hidden  relative  mb-3">
                 <input
-                  className="border-none outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1   "
+                  className="select-none border-none outline-none text-xl bg-[#F2F5FE] rounded-md px-3 pr-10 py-1   "
                   type="password"
                   placeholder="Pass"
                 />
                 <i className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer opacity-55 hover:opacity-100 transition-all ri-eye-fill"></i>
               </div>
               <div className="px-3 my-2">
-                <p className="text-xl font-bold">DATE</p>
-                <p className="text-gray-500">{new Date().toLocaleString()}</p>
+                <p className="select-none font-bold">DATE</p>
+                <p className="select-none text-gray-400 text-xs">{new Date().toLocaleString()}</p>
               </div>
               <div className="w-full h-[10%] mt-5">
                 <FancyButton text1="Create" text2="Journey" />
               </div>
+              <p className="text-sm w-full text-center mt-2 select-none">
+                Already have an account ? <Link className="text-violet-500 hover:underline underline-offset-2 cursor-pointer" to="/login">Login</Link>
+              </p>
             </div>
             {/* Ticket styles */}
             <div className="w-[10%] h-full relative ">
@@ -97,9 +103,9 @@ const Signup = () => {
             </div>
             {/* Company Text */}
             <div className="w-[15%] h-full  bg-white rounded-l-none rounded-2xl relative">
-              <div className="-rotate-90 absolute 2xl:right-[65%] sm:right-1/2  bottom-1/2 translate-y-1/2 translate-x-1/2 font-bold poppins text-[2.25vw]">
+              <div className="select-none -rotate-90 absolute 2xl:right-[65%] sm:right-1/2  bottom-1/2 translate-y-1/2 translate-x-1/2 font-bold poppins text-[2.25vw]">
                 <p>
-                  <i className="ri-vip-crown-2-fill mr-1 text-[#2663EB]"></i>
+                  <i className="select-none ri-vip-crown-2-fill mr-1 text-[#2663EB]"></i>
                   techmingle
                 </p>
               </div>
