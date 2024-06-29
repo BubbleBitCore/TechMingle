86cd7ece479690c706c0cd1115f39220c679558f
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-06-29 23:07:36 +0530

    Auto scrollbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -279,6 +279,7 @@ const Home = ({ Header }) => {
     Render.run(render);
   };
 
+  const [scrollTimer, setScrollTimer] = useState(null);
   useEffect(() => {
     // hangingImagesS1();
     initCircularText();
@@ -291,6 +292,25 @@ const Home = ({ Header }) => {
       //   s1ContainerRef.current.clientHeight;
       // window.location.reload();
     }, 300);
+
+    // handling scrollbar
+    const removeScrollBar = () => {
+      mainContainerRef.current.classList.add("hideScrollBar");
+    };
+    const addScrollBar = () => {
+      mainContainerRef.current.classList.remove("hideScrollBar");
+    };
+    mainContainerRef.current.addEventListener("scroll", () => {
+      clearTimeout(scrollTimer);
+      addScrollBar();
+    });
+    mainContainerRef.current.addEventListener("scrollend", () => {
+      setScrollTimer(
+        setTimeout(() => {
+          removeScrollBar();
+        }, 2500)
+      );
+    });
 
     window.addEventListener("resize", handleResize);
     window.addEventListener("keydown", handleKeyDowns);
@@ -593,6 +613,21 @@ const Home = ({ Header }) => {
         .carouselContainer-6 img:nth-child(6){
           filter:blur(0px) !important;
         }
+
+        /* Scrollbar */
+        html{
+          scrollbar-transition:smooth;
+        }
+        .scrollTransition::-webkit-scrollbar{
+          width:9px;
+          transition:width 1s ease ;
+        }
+        .hideScrollBar::-webkit-scrollbar{
+          width:0;
+        }
+        .scrollTransition::-webkit-scrollbar-thumb:hover{
+          cursor:pointer;
+        }
         
       `}
       </style>
@@ -600,7 +635,7 @@ const Home = ({ Header }) => {
         {/* <Header urlName="TechMingle" /> */}
         <div
           ref={mainContainerRef}
-          className="mt-1 mb-2 h-full w-full overflow-hidden overflow-y-auto snap-mandatory snap-y"
+          className="mt-1 mb-2 h-full w-full overflow-hidden overflow-y-auto scrollTransition snap-mandatory snap-y"
         >
           {/* section1 */}
           <div
@@ -742,7 +777,9 @@ const Home = ({ Header }) => {
                 } transition-all duration-200 rounded-[2rem] -top-1/2 max-sm:-top-[46%] lg:left-[6rem] md:left-[4.8rem] max-md:left-[4.8rem] max-sm:left-[1.9rem] mr-10 max-sm:mr-5 `}
               ></div>
               {/* circles */}
-              <div className={`relative w-[5rem]  lg:mr-14 md:mr-8 max-md:mr-8 max-sm:mr-6`}>
+              <div
+                className={`relative w-[5rem]  lg:mr-14 md:mr-8 max-md:mr-8 max-sm:mr-6`}
+              >
                 {/* cicle1 */}
                 <div
                   className={`rounded-full lg:w-[5.5rem] md:w-[4.5rem] max-md:w-[4.5rem] max-sm:w-[3.5rem] lg:h-[5.5rem] md:h-[4.5rem] max-md:h-[4.5rem] max-sm:h-[3.5rem] flex justify-center items-center row3Gradient`}
@@ -827,7 +864,9 @@ const Home = ({ Header }) => {
               Try it for free
             </div>
             {/* row 6 Sponsor */}
-            <div className={`flex lg:gap-5 md:gap-4 max-md:gap-4 absolute lg:bottom-10 md:bottom-14 max-md:bottom-14 left-1/2 -translate-x-1/2 z-20 pointer-events-none justify-center items-center max-sm:scale-[0.8]`}>
+            <div
+              className={`flex lg:gap-5 md:gap-4 max-md:gap-4 absolute lg:bottom-10 md:bottom-14 max-md:bottom-14 left-1/2 -translate-x-1/2 z-20 pointer-events-none justify-center items-center max-sm:scale-[0.8]`}
+            >
               <img
                 src={google}
                 className={`invert-[20%] ${
