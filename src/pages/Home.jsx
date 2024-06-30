6c507f0b6b45c64509e47362945dfe5f474adae1
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-06-30 20:05:15 +0530

    Added orient

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -346,8 +346,8 @@ const Home = ({ Header }) => {
         console.log(event.beta);
         console.log(event.gamma);
         const { beta, gamma } = event;
-        engineRef.current.gravity.x = -beta / 90;
-        engineRef.current.gravity.y = -gamma / 90;
+        engineRef.current.gravity.x = -beta / 180;
+        engineRef.current.gravity.y = -gamma / 180;
       }
     };
 
